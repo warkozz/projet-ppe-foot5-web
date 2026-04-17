50c03dc10d92f3f660dbd4011cea18e3b8aaa6d7
warkozz <hakimrayane@hotmail.fr>
2026-04-17 16:06:51 +0200

    feat: add additional CORS origins for localhost development

--- backend/main.py
+++ backend/main.py
@@ -28,6 +28,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://localhost:3001",
+        "http://127.0.0.1:3001",
         "http://localhost:8000",
     ],
     allow_credentials=True,
